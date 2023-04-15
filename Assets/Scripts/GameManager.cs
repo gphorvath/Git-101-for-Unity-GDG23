1e7a5b83e7d1d43b997e4cb0102e946ca425010f
Gregory Horvath <gphorvath@gameshenanigans.com>
2023-04-15 15:59:54 -0400

    Added TMP and scoring system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+// Borrowed code from here: https://www.awesomeinc.org/tutorials/unity-pong/
 
 public class GameManager : MonoBehaviour
 {
 
-    public static int PlayerScore1 = 0;
-    public static int PlayerScore2 = 0;
+    public static GameManager Instance { get; private set; }
 
-    public GUISkin layout;
+    public TMP_Text Player1ScoreUI;
+    public TMP_Text Player2ScoreUI;
+    public int PlayerScore1 = 0;
+    public int PlayerScore2 = 0;
 
     GameObject theBall;
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +43,18 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public static void Score(string wallID)
+    public void Score(string wallID)
     {
         if (wallID == "RightWall")
         {
             PlayerScore1++;
+            Player1ScoreUI.text = PlayerScore1.ToString();
         }
         else
         {
             PlayerScore2++;
+            Player2ScoreUI.text = PlayerScore2.ToString();
         }
     }
+
 }
